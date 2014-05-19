38ecf332c71e285c214411bc18758dbdd966447b
Chris Adams <chris.adams@jollamobile.com>
2014-05-19 16:13:05 +1000

    [sociald] Load translator for Twitter notifications. Contributes to JB#19139
    This commit ensures that the Twitter plugin loads translations as
    required.
    
    Contributes to JB#19139

--- src/twitter/twitter-notifications/twitternotificationsplugin.cpp
+++ src/twitter/twitter-notifications/twitternotificationsplugin.cpp
@@ -9,6 +9,9 @@
 #include "twittermentiontimelinesyncadaptor.h"
 #include "socialnetworksyncadaptor.h"
 
+#include <QTranslator>
+#include <QCoreApplication>
+
 extern "C" TwitterNotificationsPlugin* createPlugin(const QString& pluginName,
                                        const Buteo::SyncProfile& profile,
                                        Buteo::PluginCbInterface *callbackInterface)
@@ -28,6 +31,15 @@ TwitterNotificationsPlugin::TwitterNotificationsPlugin(const QString& pluginName
                          QStringLiteral("twitter"),
                          SocialNetworkSyncAdaptor::dataTypeName(SocialNetworkSyncAdaptor::Notifications))
 {
+    QString translationPath("/usr/share/translations/");
+    // QTranslator life-cycle: owned by ButeoSocial and removed by its own destructor
+    QTranslator *engineeringEnglish = new QTranslator(this);
+    engineeringEnglish->load("lipstick-jolla-home-twitter-notif_eng_en", translationPath);
+    QCoreApplication::instance()->installTranslator(engineeringEnglish);
+
+    QTranslator *translator = new QTranslator(this);
+    translator->load(QLocale(), "lipstick-jolla-home-twitter-notif", "-", translationPath);
+    QCoreApplication::instance()->installTranslator(translator);
 }
 
 TwitterNotificationsPlugin::~TwitterNotificationsPlugin()
